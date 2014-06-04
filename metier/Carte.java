844fd0017899e23e817522e037b87b7a36befe89
Nistof <nistof.contact@gmail.com>
2014-06-04 16:33:46 +0200

    Carte class written

--- metier/Carte.java
+++ metier/Carte.java
@@ -6,6 +6,19 @@
  * @version 1 du 04/06/2014
 **/
 
-public class Carte {
+package metier;
 
+public class Carte {
+	private static String[] COULEURS = { "ROUGE", "JAUNE", "VERT", "BLEU", "GRIS"};
+
+	private String couleur = "NULL";
+	private int valeur;
+	
+	public Carte ( String couleur, int valeur) {
+		for ( String ch : COULEURS)
+			if ( ch.equals(couleur))
+				this.couleur = couleur;
+				
+		this.valeur = valeur;
+	}
 }
