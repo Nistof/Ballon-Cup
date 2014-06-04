9a6e94012d0961f7b4812d3f393ac4219e07fd9d
Martin Florian <nistof.contact@gmail.com>
2014-06-04 21:58:44 +0200

    Crée un fichier avec l'ensemble des cartes

---
+++ util/FichierCartes.java
@@ -0,0 +1,52 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class FichierCartes {
+	private FichierCartes () {}
+	
+	public static void generer () {
+		try {
+			PrintWriter pw = new PrintWriter( new FileWriter("cartes"));
+			int j;
+			//Cartes rouges
+			for ( int i = 1; i <= 13; i++)
+				pw.append( String.format( "R%02d", i));
+			
+			//Cartes jaunes
+			j = 3;
+			for ( int i = 1; i <= 13; i++) {
+				pw.append( String.format( "J%02d", i));
+				if ( j%5 == 0)
+					i++;
+				j++;
+			}
+			
+			//Cartes vertes
+			j = 1;
+			for ( int i = 1; i <= 13; i++) {
+				pw.append( String.format( "V%02d", i));
+				if ( j%4 == 2 || j%4 == 3)
+					i++;
+				j++;
+			}
+			
+			//Cartes bleues
+			for ( int i = 1; i <= 13; i++) {
+				pw.append( String.format( "B%02d", i));
+				i++;
+			}
+			
+			//Cartes grises
+			for ( int i = 1; i <= 13; i++) {
+				pw.append( String.format( "G%02d", i));
+				i+=2;
+			}
+			
+			pw.close();
+		} catch ( Exception e) { e.printStackTrace(); }
+	}
+	
+	public static void main ( String[] args) {
+		FichierCartes.generer();
+	}
+}
