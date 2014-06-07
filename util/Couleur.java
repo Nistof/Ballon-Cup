e22c9f2f56a4e605d22c315651fe0f8af8666651
Martin Florian <nistof.contact@gmail.com>
2014-06-07 07:46:39 +0200

    Suppression du membre valeur (Inutile)

--- util/Couleur.java
+++ util/Couleur.java
@@ -9,20 +9,12 @@
 package util;
 
 public enum Couleur {
-	ROUGE ( 16711680),
-	JAUNE ( 16776960),
-	VERT  ( 65280   ),
-	BLEU  ( 255     ),
-	GRIS  ( 8421504 ),
-	DEFAUT( 0       );
-	
-	private int valeur;
-	
-	private Couleur ( int valeur) {
-		this.valeur = valeur;
-	}
-	
-	public int getValeur () { return this.valeur; }
+	ROUGE ,
+	JAUNE ,
+	VERT  ,
+	BLEU  ,
+	GRIS  ,
+	DEFAUT;
 	
 	public static Couleur getCouleur ( String c) {
 		Couleur couleur = Couleur.DEFAUT;
