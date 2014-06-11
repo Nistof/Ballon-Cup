819e585cee9ca6b900a76f32fdc2f075b8b89e52
Nistof <nistof.contact@gmail.com>
2014-06-11 16:02:53 +0200

    Ajout d'une classe de Test

---
+++ Test.java
@@ -0,0 +1,79 @@
+/* Classe: Test
+ * @author MARECAL Thomas
+ * @author QUENTIN Thibaut
+ * @author MARTIN Florian
+ * Groupe I2
+ * @version 1 du 11/06/2014
+**/
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.io.File;
+import metier.*;
+
+public class Test {
+	public static void main ( String[] args) {
+		String nom = "";
+		String fichier = "";
+		String carte, couleur;
+		int valeur;
+		Pattern p;
+		Matcher m;
+		Pioche<Carte> pioche = new Pioche<Carte>();
+		
+		try {
+			File rep = new File("ressources/");
+			
+			//Choix du fichier
+			System.out.println("Liste des fichiers disponibles pour initialiser la pioche:");
+			for ( File f : rep.listFiles()) {
+				System.out.println( f.getName());
+			}
+		
+			System.out.print("\nChoix : ");
+			
+			Scanner sc = new Scanner( System.in);
+			nom = sc.nextLine();
+			sc.close();
+		
+			//Chargement du fichier
+			sc = new Scanner( new File("ressources/" + nom));
+			if ( sc.hasNext())
+				fichier = sc.nextLine();
+			
+		} catch ( Exception e) { e.printStackTrace(); }
+		
+		p = Pattern.compile("[RVBGJ](0[1-9]|1[0-3])");
+		m = p.matcher( fichier);
+		
+		//Création des cartes
+		while ( m.find()) {
+			carte = m.group();
+			switch ( carte.charAt(0)){
+				case 'R':
+					couleur = "ROUGE";
+					break;
+				case 'G':
+					couleur = "GRIS";
+					break;
+				case 'B':
+					couleur = "BLEU";
+					break;
+				case 'V':
+					couleur = "VERT";
+					break;
+				case 'J':
+					couleur = "JAUNE";
+					break;
+				default:
+					couleur = "DEFAUT";
+			}
+			valeur = Integer.parseInt( carte.substring(1));
+			
+			pioche.ajouter( new Carte( couleur, valeur));
+		}
+		
+		System.out.println(pioche);
+	}
+}
