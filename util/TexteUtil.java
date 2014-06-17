d5ea4826dad33a30dcc4b87ea28b72d6d6e38013
Nistof <nistof.contact@gmail.com>
2014-06-17 11:08:40 +0200

    Ajout d'un utilitaire pour centrer un texte

---
+++ util/TexteUtil.java
@@ -0,0 +1,35 @@
+/* Classe: TexteUtil
+ * @author MARECAL Thomas
+ * @author QUENTIN Thibaut
+ * @author MARTIN Florian
+ * Groupe I2
+ * @version 2 du 17/06/2014
+**/
+
+package util;
+
+import java.lang.StringBuilder;
+
+public class TexteUtil {
+   private TexteUtil () {}
+   
+   //Prend en paramètre le texte à centrer et l'espace max disponible
+   public static String centrer ( String texte, int taille) {    
+      int tailleTexte = texte.length();
+      int espaceGauche = 0, espaceDroite = 0;
+      
+      if ( taille == tailleTexte || taille < tailleTexte)
+         return texte;
+      
+      espaceGauche = (int)(taille-tailleTexte)/2;
+      espaceDroite = (taille-tailleTexte)-espaceGauche;
+   
+      StringBuilder strBuild = new StringBuilder(texte);
+      for ( int i = 0; i < espaceGauche; i++)
+         strBuild.insert( 0, ' ');
+      for ( int i = 0; i < espaceDroite; i++)
+         strBuild.append( ' ');
+      
+      return strBuild.toString();
+   }
+}
