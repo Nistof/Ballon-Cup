25b162d99f902abfec4c6727f660a6498da3d780
Teenal <thibaut.quentin@laposte.net>
2014-06-17 09:57:17 +0200

    Ecriture d'un main de base pour jouer les cartes dans Jeu

--- Jeu.java
+++ Jeu.java
@@ -6,8 +6,31 @@
  * 
  */
 
+import metier.*;
+
 public class Jeu {
 
 	public static void main (String[] a) {
+		Jeu j = new Jeu();
+		while(j.continuer()) {
+			try {
+				System.out.println(j);
+				Scanner sc = new Scanner(System.in);
+				System.out.println("Joueur 1 : Jouez une carte");
+				System.out.println("Voici vos cartes : " + j.montrerMain('G'));
+				System.out.println("Choisissez l'index de la carte : ");
+				j.jouerCarte('G',sc.nextInt());
+				System.out.println(j);
+				System.out.println("Joueur 2 : Jouez une carte");
+				System.out.println("Voici vos cartes : " + j.montrerMain('D'));
+				System.out.println("Choisissez l'index de la carte : ");
+				j.jouerCarte('D',sc.nextInt());
+				System.out.println(j);
+			}
+			catch(Exception e) {
+				System.out.println(e);
+			}
+
+		}
 	}
 }
