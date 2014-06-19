90db2c53e6f3af7c04290961eaeb55e9fb609cc8
Martin Florian <nistof.contact@gmail.com>
2014-06-19 22:05:13 +0200

    Creation d'une interface

---
+++ ihm/Ihm.java
@@ -0,0 +1,12 @@
+/* Interface: Ihm
+ * @author MARECAL Thomas
+ * @author MARTIN Florian
+ * @author QUENTIN Thibaut 
+ * Groupe I2
+ * @version 1 du 17/06/2014
+ */
+ 
+public interface Ihm {
+	public void afficherTuiles( );
+	public void afficherJoueur( int i );
+}
