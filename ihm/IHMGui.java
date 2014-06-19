f2b99ce1991d528755789f0e747dafbf2718f856
Thibaut Quentin <thibaut.quentin@laposte.net>
2014-06-19 18:50:42 +0200

    Ajout de la classe IHMGui (à completer)

---
+++ ihm/IHMGui.java
@@ -0,0 +1,24 @@
+/* IHMGui
+ * @author MARTIN Florian
+ * @author MARECAL Thomas
+ * @author QUENTIN Thibaut
+ * Groupe I2
+ */
+
+package ihm;
+import metier.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class IHMGui extends JFrame {
+
+	private Jeu jeu;
+
+
+	public IHMGui (Jeu jeu) {
+		this.jeu = jeu;
+		add
+	}
+}
